79549fa13f7c4b064183d7844f547380ad02e186
Tyrik <127711450+Tyritic@users.noreply.github.com>
2023-11-21 16:01:57 +0800

    Add files via upload

---
+++ 图论算法/广度优先搜索/邻接表表示/Test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"Graph.h"
+using namespace std;
+int main()
+{
+	Graph test(5);
+	test.initVertex();
+	test.addEdge(0, 1);
+	test.addEdge(0, 3);
+	test.addEdge(1, 3);
+	test.addEdge(1, 2);
+	test.addEdge(2, 4);
+	test.addEdge(3, 2);
+	test.addEdge(3, 4);
+	test.display();
+	cout << endl;
+	//for (int i = 0; i < test.numVer; i++)
+		//cout << test.inDegree[i] << endl;
+	test.TopologicalSort();
+	
+}
